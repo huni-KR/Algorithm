77270d876f1786a3c955ffef98325bd44b7950c4
Tae Hun Kim <xogns12356@gmail.com>
2020-10-29 15:51:54 +0900

    17618번 : 신기한 수

--- Beakjoon/BOJ_17618/BOJ_17618.java
+++ Beakjoon/BOJ_17618/BOJ_17618.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 public class BOJ_17618 {
 
     public static void main(String[] args) {
-        System.out.println("HELLO");
+
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        kb.close();
+
+                            
+        int n = Integer.parseInt( str );
+        int count = 0;
+        for(int i=1; i<=n; i++){
+            int num = fun(Integer.toString(i));
+            if( i % num == 0 )
+                count++;
+        }
+        
+        System.out.println( count );
+
     }
 
+    private static int fun(String str) {
+        int result = 0;
+        for(int i=0; i<str.length(); i++ )
+            result += str.charAt(i) - '0';
+        return result;
+    }
 }
