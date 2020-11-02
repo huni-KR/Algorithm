0e41df5eafc2c3cbc1e0952ac983d160bf37a740
Tae Hun Kim <xogns12356@gmail.com>
2020-11-02 10:27:02 +0900

    BOJ_18229

---
+++ Beakjoon/BOJ_18229/BOJ_18229.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer( br.readLine(), " " );
+        int n = Integer.parseInt( st.nextToken() );
+        int m = Integer.parseInt( st.nextToken() );
+        int k = Integer.parseInt( st.nextToken() );
+
+        int[][] grid = new int[n][m];
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer( br.readLine(), " " );
+            for(int j=0; j<m; j++)
+                grid[i][j] = Integer.parseInt( st.nextToken() );
+        }
+
+        int[] sum = new int[n];
+        int i = 0, j = 0;
+        boolean flag = true;
+        for(i=0; i<m; i++){
+            for(j=0; j<n; j++){
+                sum[j] += grid[j][i];
+                if( sum[j] >= k ){
+                    flag = false;
+                    break;
+                }   
+
+            }
+            if( !flag )
+                break;
+        }
+        System.out.println( (j+1) + " " + (i+1) );
+
+    }
+
+}
