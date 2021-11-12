46693938fc2c284221f73f9f9ec3fda5ce3140fc
huni <xogns12356@gmail.com>
2021-11-12 15:59:19 +0900

    백준 16940 풀이

--- Beakjoon/BOJ_16940/BOJ_16940.java
+++ Beakjoon/BOJ_16940/BOJ_16940.java
@@ -4,88 +4,64 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ_16940 {
-    
-    static Map map;
-    static List[] list;
-    static boolean[] visit;
-    static Queue queue;
-    static Queue result;
-    public static void main(String[] args) throws NumberFormatException, IOException{
-        
-        result = new LinkedList<>();
-        map = new HashMap<Integer, Integer>();
+
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        visit = new boolean[n+1];
-        list = new ArrayList[n+1];
-        for(int i=1; i<list.length; i++){
+
+        ArrayList[] list = new ArrayList[n + 1];
+        for (int i = 0; i < list.length; i++) {
             list[i] = new ArrayList<Integer>();
         }
 
-        for(int i=0; i<n-1; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            list[s].add(e);
-            list[e].add(s);
+            list[a].add(b);
+            list[b].add(a);
         }
-       
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-        for(int i=0; i<n; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = Integer.parseInt(st.nextToken());
         }
-        
-        // bfs(arr[0], 1);
-        
-        // for(int i=0; i<n; i++){
-        //     arr[i] = (int)map.get(arr[i]);
-        // }
 
-        boolean flag = isBFS(arr);
-
-        System.out.println( flag ? 1 : 0);
-
-        // for(int i=0; i<n; i++){
-        //     System.out.print(arr[i] + " ");
-        // }
-
+        System.out.println(BFS(list, answer, 1) ? 1 : 0);
     }
-    private static boolean isBFS(int[] arr) {
-        result.add(arr[0]);
-        visit[arr[0]] = true;
 
-        
+    private static boolean BFS(ArrayList[] list, int[] answer, int start) {
+        boolean[] visit = new boolean[list.length];
+        HashSet<Integer> set = new HashSet<>();
 
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visit[start] = true;
 
+        while (!queue.isEmpty()) {
+            set.clear();
+            int q = queue.poll();
+            for (int i = 0; i < list[q].size(); i++) {
+                int next = (int) list[q].get(i);
+                if (!visit[next]) {
+                    visit[next] = true;
+                    set.add(next);
+                }
+            }
 
-        
+            int size = set.size();
+            for (int i = start; i < start + size; i++) {
+                if (set.contains(answer[i])) {
+                    queue.add(answer[i]);
+                } else {
+                    return false;
+                }
+            }
+            start += size;
+        }
         return true;
     }
-    // private static void bfs(int pos, int level) {
-        
-    //     visit[pos] = true;
-
-    //     if( map.get(pos) == null){
-    //         map.put(pos, level);
-    //     }
-        
-
-    //     queue = new LinkedList<Integer>();
-    //     queue.add(pos);
-
-    //     while(!queue.isEmpty()){
-
-    //         int now = (int) queue.poll();
-
-    //         for(int i=0; i<list[now].size(); i++){
-    //             if( !visit[(int) list[now].get(i)] ){
-    //                 bfs((int) list[now].get(i), level+1);
-    //             }
-    //         }
-
-
-    //     }
-    // }    
 }
