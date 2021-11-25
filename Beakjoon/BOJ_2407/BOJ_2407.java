6f32e2f30e4d67202cf692d9c388257997124fa0
huni <xogns12356@gmail.com>
2021-11-25 09:23:33 +0900

    백준 2407 풀이

---
+++ Beakjoon/BOJ_2407/BOJ_2407.java
@@ -0,0 +1,23 @@
+package BOJ_2407;
+
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class BOJ_2407 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        BigInteger a = BigInteger.ONE;
+        BigInteger b = BigInteger.ONE;
+        for (int i = 0; i < m; i++) {
+            a = a.multiply(new BigInteger(String.valueOf(n - i)));
+            b = b.multiply(new BigInteger(String.valueOf(i + 1)));
+        }
+        System.out.println(a.divide(b));
+    }
+}
