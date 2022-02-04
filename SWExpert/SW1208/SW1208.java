c510410fae8e615ae677e2f74422a61ae42b231c
huni <xogns12356@gmail.com>
2022-02-04 17:40:33 +0900

    SW1208

---
+++ SWExpert/SW1208/SW1208.java
@@ -0,0 +1,38 @@
+package SW1208;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class SW1208 {
+
+    static int pos;
+    static int[] dx = { 1, 0, -1, 0 };
+    static int[] dy = { 0, 1, 0, -1 };
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = 10;
+        for (int t = 0; t < T; t++) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[100];
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+            for (int i = 0; i < n; i++) {
+                arr[0]++;
+                arr[99]--;
+                Arrays.sort(arr);
+            }
+            bw.write("#" + Integer.toString(t + 1) + " " + Integer.toString(arr[99] - arr[0]) + "\n");
+        }
+        bw.flush();
+    }
+}
