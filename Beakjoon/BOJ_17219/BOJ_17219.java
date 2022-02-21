570674fe2e159892fa12d06f2356b48c4ec24e48
huni <xogns12356@gmail.com>
2022-02-21 12:40:46 +0900

    220220

---
+++ Beakjoon/BOJ_17219/BOJ_17219.java
@@ -0,0 +1,37 @@
+package BOJ_17219;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class BOJ_17219 {
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        Map<String, String> map = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            map.put(st.nextToken(), st.nextToken());
+        }
+
+        for (int i = 0; i < M; i++) {
+            bw.write(map.get(br.readLine()));
+            bw.write("\n");
+        }
+        bw.flush();
+    }
+}
